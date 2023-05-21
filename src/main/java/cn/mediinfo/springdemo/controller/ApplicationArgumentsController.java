109028337ac8eae6924084509c3779900eae089c
love75hu@hotmail.com <love75hu@hotmail.com>
2023-05-21 12:36:51 +0800

    feat:springboot 命令行参数获取demo

---
+++ src/main/java/cn/mediinfo/springdemo/controller/ApplicationArgumentsController.java
@@ -0,0 +1,32 @@
+package cn.mediinfo.springdemo.controller;
+
+import cn.mediinfo.springdemo.config.properties.MemberProperties;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("api/v1/ApplicationArguments")
+public class ApplicationArgumentsController {
+
+    /**
+     * 获取命令行参数
+     * java -jar springdemo.jar --server.port=8081
+     */
+    @Autowired
+    private ApplicationArguments args;
+    /**
+     * 获取memberProperties配置参数
+     * @return
+     */
+    @Operation(summary = "获取获取命令行参数值")
+    @GetMapping("get")
+    public ApplicationArguments Get() {
+        return args;
+    }
+}
