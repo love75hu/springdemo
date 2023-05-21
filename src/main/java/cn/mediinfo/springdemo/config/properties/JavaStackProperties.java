a0fabf73d8f6eaa35a0a29c93c1bc368f3e278c1
love75hu@hotmail.com <love75hu@hotmail.com>
2023-05-21 18:57:35 +0800

    feat:springboot profile demo

--- src/main/java/cn/mediinfo/springdemo/config/properties/JavaStackProperties.java
+++ src/main/java/cn/mediinfo/springdemo/config/properties/JavaStackProperties.java
@@ -3,6 +3,7 @@ package cn.mediinfo.springdemo.config.properties;
 import lombok.Data;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Profile;
 
 import java.util.List;
 import java.util.Map;
@@ -11,9 +12,10 @@ import java.util.Map;
  * java参数绑定示例
  */
 @Data
+@Profile("dev") //表示只有在dev环境才会被激活
 @ConfigurationProperties(prefix = "javastack")
 public class JavaStackProperties {
-    @NotNull
+    //@NotNull
     private String name;
     private List<String> user;
     private Map<String, String> params;
--- src/main/java/cn/mediinfo/springdemo/controller/JavaStackController.java
+++ src/main/java/cn/mediinfo/springdemo/controller/JavaStackController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
