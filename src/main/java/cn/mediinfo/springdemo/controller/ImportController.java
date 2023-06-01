d8f09184a7791bb437d76033b39e70c31499665b
love75hu@hotmail.com <love75hu@hotmail.com>
2023-06-01 22:19:02 +0800

    feat:自定义拦截器

--- src/main/java/cn/mediinfo/springdemo/controller/ImportController.java
+++ src/main/java/cn/mediinfo/springdemo/controller/ImportController.java
@@ -1,18 +1,13 @@
 package cn.mediinfo.springdemo.controller;
 
-import cn.mediinfo.springdemo.config.properties.MemberProperties;
 import cn.mediinfo.springdemo.response.MsfResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.models.OpenAPI;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.JpaRepositoryConfigExtension;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.sql.DataSource;
 
 /**
  * 导入配置
@@ -20,7 +15,7 @@ import javax.sql.DataSource;
 @RestController
 @Slf4j
 @RequestMapping("api/v1/Import")
-public class ImportController {
+public class ImportController{
     @Autowired
     private OpenAPI openAPI;
     /**
